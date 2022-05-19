49079e2d5d8a40f3e514d2b36935ca36e0315fe6
Kiet112345 <vankietclbk@gmail.com>
2022-05-19 17:34:23 +0700

    proxy

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=database-558623736876
+export POSTGRES_USERNAME=kiet
 export POSTGRES_PASSWORD=kiet12345
-export POSTGRES_HOST=database-558623736876.c4fdy79yr8ga.us-west-1.rds.amazonaws.com
-export POSTGRES_DB=postgresSQL
-export AWS_BUCKET=arn:aws:iam::558623736876:user/kiet_project3
+export POSTGRES_HOST=pgr-558623736876.c4fdy79yr8ga.us-west-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test1-558623736876-dev
 export AWS_REGION=us-west-2
-export AWS_PROFILE=test
+export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
