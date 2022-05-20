4e8b75a67b0869cef928dd554d3cff68c7fe4af2
KIET112345 <vankietclbk@gmail.com>
2022-05-20 17:18:31 +0700

    abc

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=kiet
 export POSTGRES_PASSWORD=kiet12345
-export POSTGRES_HOST=pgr-558623736876.c4fdy79yr8ga.us-west-1.rds.amazonaws.com
+export POSTGRES_HOST=db-558623736876.c4fdy79yr8ga.us-west-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=test1-558623736876-dev
 export AWS_REGION=us-west-2
