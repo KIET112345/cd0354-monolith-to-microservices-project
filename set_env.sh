08bcb1cfcdf193170ac172cdf36345cc329596cd
KIET112345 <vankietclbk@gmail.com>
2022-05-30 16:52:57 +0700

    fixAutoscaling

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,8 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=kiet
+export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=kiet12345
-export POSTGRES_HOST=db-558623736876.c4fdy79yr8ga.us-west-1.rds.amazonaws.com
+export POSTGRES_HOST=db-558623736876.coesqtmeip1w.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=test1-558623736876-dev
 export AWS_REGION=us-west-2
